5dab9df30f1f038a2374517fc84b056bc16a81d7
Clouds Flowing <clouds.flowing@gmail.com>
2018-07-19 13:05:22 +0800

    init viewer

---
+++ lib/jx3replay.ex
@@ -0,0 +1,18 @@
+defmodule Jx3replay do
+  @moduledoc """
+  Documentation for Jx3replay.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Jx3replay.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/jx3replay_test.exs
@@ -0,0 +1,8 @@
+defmodule Jx3replayTest do
+  use ExUnit.Case
+  doctest Jx3replay
+
+  test "greets the world" do
+    assert Jx3replay.hello() == :world
+  end
+end
